7698d9e8013e2df8d2660b69ddb3d99e132a6104
mariaelguli-hub <mariaelguli@gmail.com>
2026-08-08 00:39:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import TermsConditions from './pages/TermsConditions'
 import RefundPolicy from './pages/RefundPolicy'
 import NotFound from './pages/NotFound'
 
-function App() {
+export default function App() {
   return (
     <>
       <Toaster position="top-right" toastOptions={{
@@ -30,14 +30,21 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/faq" element={<FAQ />} />
+          
+          {/* Legal Routes */}
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-conditions" element={<TermsConditions />} />
           <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="/return-policy" element={<RefundPolicy />} />
+          <Route path="/cookie-policy" element={<PrivacyPolicy />} />
+          <Route path="/gdpr-policy" element={<PrivacyPolicy />} />
+          <Route path="/legal-notice" element={<TermsConditions />} />
+          <Route path="/shipping-delivery" element={<FAQ />} />
+          <Route path="/disclaimer" element={<TermsConditions />} />
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
   )
 }
-
-export default App
