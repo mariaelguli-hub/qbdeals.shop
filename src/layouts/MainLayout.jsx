6f5a3307d834bdaa493bbaef5498f3fd2070cf94
mariaelguli-hub <mariaelguli@gmail.com>
2026-08-08 02:37:33 +0100

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,21 @@
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import AnnouncementBar from '../components/AnnouncementBar'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import ScrollToTop from '../components/ScrollToTop'
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <AnnouncementBar />
+    <div className="min-h-screen flex flex-col bg-white text-gray-900 font-sans antialiased">
+      {/* Header (Fih TopBar Pro Max) */}
       <Header />
-      <main className="flex-1">
+
+      {/* Main Content Area */}
+      <main className="flex-grow">
         <Outlet />
       </main>
+
+      {/* Footer */}
       <Footer />
-      <ScrollToTop />
     </div>
   )
 }
